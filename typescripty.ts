2fd45cbe4f1f988b826a9a6772cc9e67718f36e3
Gedeotwiz <gedeontwizerimana6@gmail.com>
2024-08-12 13:46:45 +0200

    study what type is and how and when it is used

---
+++ typescripty.ts
@@ -0,0 +1,9 @@
+
+
+//Types are used to define the shape of variables, functions, classes, etc. They provide a way to enforce what kind of values a variable can hold.
+
+type john={
+    name:string,
+    age:number,
+    player:boolean
+}
