c8dd07a3febd5dfb48621280a758566729b0d1eb
Gedeotwiz <gedeontwizerimana6@gmail.com>
2024-08-12 15:39:44 +0200

    study hw tuples work in typescripty

---
+++ tuples.js
@@ -0,0 +1,4 @@
+//Tuples are a special type in TypeScript that allows you to create an array with a fixed number of elements, where each element can have a different type.
+var tuple;
+tuple = ["Alice", 30, true];
+console.log(tuple);
